d045d7ed056f82b810e873e99c7e26bbaaca2ea4
Mateus Araujo <empresarial.mateus25@gmail.com>
2026-07-19 20:09:06 -0300

    feat(backend): Adiciona rota health ao servidor

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,16 @@
-console.log("hello world");
+import express from "express";
+import "dotenv/config"
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({
+        status: "ok"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("SERVIDOR RODANDO NA PORTA", PORT)
+});
