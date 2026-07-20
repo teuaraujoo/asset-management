7ad12721266abe830c8758a2f186675d902cac3f
Mateus Araujo <empresarial.mateus25@gmail.com>
2026-07-20 13:41:55 -0300

    test: Modifica mensagem do frontend para testar deploy netlify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-// @ts-ignore: side-effect import for CSS
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Hello World, this project will be a asset management system</h1>
     </>
   )
 }
