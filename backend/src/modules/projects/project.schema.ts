708b98e84472d5c4504a5822e1bac412e717e070
Mateus Araujo <empresarial.mateus25@gmail.com>
2026-07-27 19:14:53 -0300

    refactor(projects): Modifica metodos de projetos service e modifica schema de projetos
    Esse commit cria um metodo no folder service, que e um servico intenor, que sera usado pelo project service inicialmente. Além disso, modifica os metodos do service de projeto e agora eles sao assincronos e atualiza o schema de projeto e o id agora é string

--- backend/src/modules/projects/project.schema.ts
+++ backend/src/modules/projects/project.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const createProjectSchema = z.object({
-    user_id: z.int().positive(),
-    folder_id: z.int().positive(),
+    user_id: z.string().nonoptional(),
+    folder_id: z.string().nonoptional(),
     name: z.string().min(5, "O nome do projeto deve ter pelo menos 5 caracteres.").max(120, "O nome do projeto não pode passar de 120 caracteres."),
     mini_description: z.string().max(255, "A mini descrição do projeto deve ter no máximo 255 caracteres."),
     description: z.string()
--- backend/src/modules/projects/projects.services.ts
+++ backend/src/modules/projects/projects.services.ts
@@ -1,25 +1,26 @@
 import AppError from "../../error/app-error";
+import FolderRepository from "../folders/folders.services";
 import { CreateProjectBody, createProjectSchema } from "./project.schema";
 import ProjectRepository from "./projects.repositories";
 
 export default class ProjectService {
-    static createProject(body: CreateProjectBody) { 
+    static async createProject(body: CreateProjectBody) {
         const data = createProjectSchema.parse(body);
-        const folder = FolderRepository.get(data.folder_id);
+        const folder = await FolderRepository.getById(data.folder_id);
 
         if (folder) throw new AppError("Já existe um projeto vinculado a essa pasta.", 409);
 
     };
 
-    static updateProject() { 
-        
-    };
-
-    static deleteProject() { 
+    static async updateProject() {
 
     };
 
-    private generateProjectSlug() { 
+    static async deleteProject() {
+
+    };
+
+    private async generateProjectSlug() {
 
     };
 };
