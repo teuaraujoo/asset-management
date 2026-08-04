4679ed74220823cdfd539b6df191e6428b6645b3
Mateus Araujo <empresarial.mateus25@gmail.com>
2026-08-04 12:53:02 -0300

    refactor(auth): Remove estado de loading de rota publica

--- frontend/src/routes/PublicRoute.tsx
+++ frontend/src/routes/PublicRoute.tsx
@@ -1,13 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "@/hooks/auth/use-auth";
-import LoadingAuth from "../components/auth/LoadingAuth";
 
 export default function PublicRoute() {
-    const { loading, isAuth } = useAuth();
-
-    if (loading) {
-        return <LoadingAuth />
-    };
+    const { isAuth } = useAuth();
 
     return isAuth ? <Navigate to="/dashboard/home" /> : <Outlet />;
 };
