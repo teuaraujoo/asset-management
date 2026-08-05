3bbfa86f9cdac3d552dfd0b778035454e5d36ae4
Mateus Araujo <empresarial.mateus25@gmail.com>
2026-08-05 16:11:49 -0300

    chore: Remove estado de loading de rotas publicas

--- frontend/src/routes/PublicRoute.tsx
+++ frontend/src/routes/PublicRoute.tsx
@@ -1,13 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
-import LoadingAuth from "@/components/auth/LoadingAuth";
 import { useAuthContext } from "@/contexts/AuthContext";
 
 export default function PublicRoute() {
-    const { loading, isAuth } = useAuthContext();
-
-    if (loading) {
-        return <LoadingAuth />;
-    };
+    const { isAuth } = useAuthContext();
 
     return isAuth ? <Navigate to="/dashboard/home" /> : <Outlet />;
 };
